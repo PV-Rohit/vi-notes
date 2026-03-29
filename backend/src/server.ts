f68524f08cd1cac0e9967621f80280696f8b8246
PV-Rohit <323103382043@gvpce.ac.in>
2026-03-29 10:51:59 +0530

    modified db from local storage to mongodb atlas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,11 +15,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/sessions', sessionRoutes);
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/vi-notes';
+const MONGO_URI = process.env.MONGO_URI || '';
 
 mongoose.connect(MONGO_URI)
     .then(() => {
-        console.log('Connected to MongoDB');
+        console.log(`Connected to MongoDB ${MONGO_URI}`);
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
     .catch(err => console.error('MongoDB connection error:', err));
