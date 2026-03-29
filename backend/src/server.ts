c2f1874fea06bcb03e439f6177377bb6c3a3c24f
PV-Rohit <323103382043@gvpce.ac.in>
2026-03-29 14:25:02 +0530

    modified server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,11 +17,17 @@ app.use('/api/sessions', sessionRoutes);
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || '';
 
+app.use("/", (req, res) => {
+    res.send("Hello World")
+})
+
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-mongoose.connect(MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
